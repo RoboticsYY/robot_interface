4a40b7504c250a9cb106cdbd870629cb68d53f8e
RoboticsYY <yu.yan@intel.com>
2019-09-17 18:23:31 +0800

    Add control base class header file

---
+++ include/robot_interface/control_base.hpp
@@ -0,0 +1,53 @@
+// Copyright (c) 2019 Intel Corporation. All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+
+class ArmControlBase: public rclcpp::Node
+{
+public:
+
+  ArmControlBase(const std::string node_name)
+  : Node(node_name)
+  {
+    joint_state_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("/joint_states");
+  }
+
+  ~ArmControlBase()
+  {
+  }
+
+  virtual bool moveToTcpPose(double x, double y, double z, 
+                             double alpha, double beta, double gamma, 
+                             double vel, double acc) = 0;
+
+  virtual bool open(const double distance) = 0;
+
+  virtual bool close(const double distance) = 0;
+
+  virtual bool pick(double x, double y, double z, 
+                    double alpha, double beta, double gamma, 
+                    double vel, double acc, double vel_scale, double approach) = 0;
+  
+  virtual bool place(double x, double y, double z, 
+                     double alpha, double beta, double gamma,
+                     double vel, double acc, double vel_scale, double retract) = 0;
+
+protected:
+  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_publisher_;
+  std::vector<std::string> joint_names_;
+};
